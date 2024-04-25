f86ef77ce54048520bc1ffc0d2b5c470c1874d8a
PC_ALEXIS\alexis <alexis.bessac@gmail.com>
2024-04-25 13:12:21 +0200

    new commit

---
+++ src/app/Page404/Page404.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-404',
+  standalone: true,
+  imports: [],
+  templateUrl: './Page404.component.html',
+  styleUrl: './Page404.component.scss'
+})
+export class Page404Component {
+
+}
